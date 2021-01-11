ac89942978d9089f1971f639bba2b12814e4802c
abhi887 <vyasa887@gmail.com>
2021-01-12 02:03:53 +0530

    added: ss area selection gui

---
+++ areaselector.py
@@ -0,0 +1,53 @@
+import tkinter as tk
+import math
+
+class areaSelector:
+    root = tk.Tk()
+    
+    # root(window) configs
+    # root.geometry("1000x600")
+    root.wm_attributes('-alpha', 0.7)
+    root.configure(bg="black")
+    root.minsize(500,500)
+    root.title("Capture Area selection Widget")
+    root.update_idletasks()
+
+    # screen cordinates for bunk-inator
+    areaStartX = 0
+    areaStartY = 0
+    areaWidth = 1920
+    areaHeight = 1080
+    
+    def __init__(self):
+        instructions = '''
+        Bunk-inator capture area selection widget (BICASW)
+        -------------------------------------------------
+        \n\n\n\n\n\n
+        Cover the area you wish to capture with this window
+        including window title bar and hit enter when done
+        \n\n\n\n\n\n
+        
+        -------------------------
+        This is not accurate to the pixel, screenshot
+        might be 10-20 pixels away from covered area
+        '''
+        tk.Label(self.root,text=instructions,font=("Arial", 13),bg="black",fg="white").place(relx=0.5,rely=0.5,anchor="center")
+        self.root.bind("<Configure>", self.configureHandler)
+        self.root.bind("<Return>", lambda _ : self.root.destroy())
+        self.root.mainloop()
+    
+    def configureHandler(self,e):
+        '''for updating cordinates each time window is resized or repositioned''' 
+        self.areaStartX=self.root.winfo_x()+8
+        self.areaStartY=self.root.winfo_y()+8
+        self.areaWidth=self.root.winfo_width()+(96*(math.ceil(self.root.winfo_width()/480)))
+        self.areaHeight=self.root.winfo_height()+(29*(math.floor(self.root.winfo_height()/75)))
+
+    def getCaptureArea(self):
+        '''for fetching cordinates in bunk-inator'''
+        return({
+            "x":self.areaStartX,
+            "y":self.areaStartY,
+            "width":self.areaWidth,
+            "height":self.areaHeight,
+        })
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from PIL import ImageGrab,Image
 from datetime import datetime
+from areaselector import areaSelector
 import numpy as np
 import os
 import time
+
 
 global slideCount,directoryName
 slideCount = 1
@@ -14,12 +16,14 @@ waitBeforeCaptureStart = 6      # will wait x-1 secs before starting script
 frequency = 1                   # will capture every x secs
 atol = 40                       # absolute tolerance for comparing new capture with last capture
 rtol = 43                       # relative tolerance for comparing new capture with last capture
+useAreaSelector = True          # flag for whether to use gui for capture area selection
+
 captureAreaX = 0                # capture area's starting x position on screen
 captureAreaY = 0                # capture area's starting y position on screen
 captureAreaWidth = 1920         # width of capture area in pixels
 captureAreaHeight = 1080        # height of capture area in pixels
 
-# uncomment anyone of the following slideArea to fit your capture area
+# uncomment anyone of the following slideArea to fit your capture area if useAreaSelector is false
 #slideArea = (captureAreaX,captureAreaY,captureAreaWidth,captureAreaHeight)        # area of capture for fullscreen 1920 x 1080 screen
 #slideArea = (0,130,1920,1020)                                                     # screen coordinates for chrome 1920 x 1080 screen
 slideArea = (0,90,1920,1020)                                                       # screen coordinates for firefox 1920 x 1080 screen
@@ -29,18 +33,31 @@ def main():
     
     if slideCount > 1:
         newSlide = ImageGrab.grab(bbox=slideArea)
-
+    
+    # session and 1st iteration setup
     if slideCount == 1:
+        # display app banner
         putBanner()
-        newDirectoryName = input("Enter directory name[optional] : ")
+
+        # use area selection gui according to flag
+        if useAreaSelector:
+            print("\rWaiting for Capture area selection ...",end="")
+            areaSelectorGui = areaSelector()
+            setCaptureArea(areaSelectorGui.getCaptureArea())
+
+        # create new directory for storing screenshots
+        newDirectoryName = input("\r\bEnter directory name[optional]  :     ")
         if newDirectoryName.strip() != "":
             directoryName = f"{baseDir}\\{newDirectoryName}"
         os.system(f"mkdir {directoryName}")
         
+        # create a delay in capture launch for user to get ready
         for i in range(1,waitBeforeCaptureStart):
             print(f"\rSlide capture will start in {waitBeforeCaptureStart-i}sec ...",end="")
             time.sleep(1)
-        
+        print("\n[!] press ctrl + c to stop ")
+
+        # capture and store initial slide/frame/screenshot
         newSlide = ImageGrab.grab(bbox=slideArea)
         newSlide.save(f"{directoryName}\{slideCount}.png")
         statusMsg = f"\r\b[+] {slideCount} slides Captured ..."
@@ -55,6 +72,7 @@ def main():
 
 
 def slideIsChanged(newSlide):
+    '''for checking if the new capture is different than the last one'''
     global slideCount,directoryName
     oldSlide = Image.open(f"{directoryName}\{slideCount-1}.png")
     oldSlideArray = np.array(oldSlide)
@@ -64,8 +82,18 @@ def slideIsChanged(newSlide):
     else:
         return True
 
+def setCaptureArea(newSlideArea):
+    '''for setting new capture area cordinates if area selection gui is used'''
+    global slideArea
+    slideArea = (
+        newSlideArea['x'],
+        newSlideArea['y'],
+        newSlideArea['width'],
+        newSlideArea['height'],
+    )
 
 def putBanner():
+    '''for displaying app banner'''
     os.system("cls") if os.name == "nt" else os.system("clear")
 
     banner = '''
@@ -81,6 +109,7 @@ def putBanner():
     print(banner)
                                                                                                 
 
+'''main loop'''
 while True:
     try:
         main()
